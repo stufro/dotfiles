64a262ca9681b63e6392d5cd09e02a9525330abc
Stuart Frost <stuart.2.frost@bt.com>
2024-06-05 20:38:33 +0100

    Add pckr lock file

---
+++ nvim/pckr/lockfile.lua
@@ -0,0 +1,18 @@
+return {
+  ["https://github.com/MunifTanjim/nui.nvim"] = { commit = "322978c734866996274467de084a95e4f9b5e0b1" },
+  ["https://github.com/gmr458/vscode_modern_theme.nvim"] = { commit = "f90402f936654611bc3a74cc0ba225cbb44a1e9c" },
+  ["https://github.com/lewis6991/gitsigns.nvim"] = { commit = "379a5a915aea3e9ba0d4a778e8478584ac0f9011" },
+  ["https://github.com/mechatroner/rainbow_csv"] = { commit = "bb2f154fd9c2bedb6513aa533baa7769e57874f2" },
+  ["https://github.com/neoclide/coc.nvim"] = { commit = "f0ce9ae23d6ce9d0cbabe73bdb738e45accc6f08" },
+  ["https://github.com/nvim-lua/plenary.nvim"] = { commit = "a3e3bc82a3f95c5ed0d7201546d5d2c19b20d683" },
+  ["https://github.com/nvim-neo-tree/neo-tree.nvim"] = { commit = "29f7c215332ba95e470811c380ddbce2cebe2af4" },
+  ["https://github.com/nvim-telescope/telescope.nvim"] = { commit = "dfa230be84a044e7f546a6c2b0a403c739732b86" },
+  ["https://github.com/nvim-tree/nvim-web-devicons"] = { commit = "b77921fdc44833c994fdb389d658ccbce5490c16" },
+  ["https://github.com/nvimdev/dashboard-nvim"] = { commit = "5346d023afc4bfc7ff63d05c70bcdb0784bb657a" },
+  ["https://github.com/slim-template/vim-slim"] = { commit = "a0a57f75f20a03d5fa798484743e98f4af623926" },
+  ["https://github.com/thoughtbot/vim-rspec"] = { commit = "52a72592b6128f4ef1557bc6e2e3eb014d8b2d38" },
+  ["https://github.com/tpope/vim-endwise"] = { commit = "3719ffddb5e42bf67b55b2183d7a6fb8d3e5a2b8" },
+  ["https://github.com/tpope/vim-rails"] = { commit = "8972461e64c7c4bf049f2f86ea1bc571e8077b55" },
+  ["https://github.com/vim-crystal/vim-crystal"] = { commit = "dc21188ec8c2ee77bb81dffca02e1a29d87cfd9f" },
+  ["https://github.com/windwp/nvim-autopairs"] = { commit = "c15de7e7981f1111642e7e53799e1211d4606cb9" },
+}
